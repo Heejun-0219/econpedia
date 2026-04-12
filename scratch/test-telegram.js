01c6b08073fcc036564fca8fd5dff95a31a241dc
Heejun-0219 <heejun0219@gmail.com>
2026-04-12 17:31:38 +0900

    chore: update site URL in astro config and cleanup contact and advertise pages

--- scratch/test-telegram.js
+++ scratch/test-telegram.js
@@ -4,7 +4,7 @@ async function test() {
   console.log('🚀 텔레그램 알림 테스트 시작...');
   const result = await publishToTelegram(
     '[테스트] EconPedia 시스템 알림',
-    'https://econpedia.kr/blog/test-article'
+    'https://econpedia.dedyn.io/blog/test-article'
   );
   console.log(result);
 }
